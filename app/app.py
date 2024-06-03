d147af8bbd1ea1e61a838e980c82419536b00830
Introspectation <mert.topkaya1996@gmail.com>
2024-06-03 13:32:22 +0300

    facecam is working. NOTE!: READ THE DEF RUN FUNCTION COMMENT TO CHANGE CAM!1!

--- app/app.py
+++ app/app.py
@@ -16,7 +16,8 @@ class VideoCapture(qtc.QThread):
         
     def run(self):
         #video capture kısmında gene arkadaki işlemler gibi değiştirmeyi unutma!
-        cap = cv2.VideoCapture(0)
+        droidcam_url = "http://192.168.1.101:4747/video"
+        cap = cv2.VideoCapture(droidcam_url)
         
         while self.run_flag:
             ret, frame = cap.read() #BGR format
@@ -41,7 +42,7 @@ class mainWindow(qtw.QWidget):
         headerLabel = qtw.QLabel('<h2>Face Mask Recognition Application</h2>')
         
         #Adding widgets for camera
-        self.cameraButton = qtw.QPushButton('Open Camera', self)
+        self.cameraButton = qtw.QPushButton('Open Camera', clicked=self.cameraButtonClick, checkable=True)
         self.cameraButton.setStyleSheet("""
             QPushButton {
                 background-color: Blue;
@@ -88,6 +89,36 @@ class mainWindow(qtw.QWidget):
         self.setLayout(layout)
         self.show()
         
+        
+    def cameraButtonClick(self):
+        print('clicked')
+        status = self.cameraButton.isChecked()
+        if status == True:
+            self.cameraButton.setText('Closed Camera')
+            
+            # open the camera
+            self.capture = VideoCapture()
+            self.capture.change_pixmap_signal.connect(self.updateImage)
+            self.capture.start()
+            
+            
+        elif status == False:
+            self.cameraButton.setText('Open Camera')
+            self.capture.stop()
+            
+    
+    @qtc.pyqtSlot(np.ndarray)        
+    def updateImage(self, image_array):
+        rgb_img = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_img.shape
+        bytes_per_line = ch*w
+        #convert into QImage
+        convertedImage = qtg.QImage(rgb_img.data, w, h, bytes_per_line, qtg.QImage.Format_RGB888)
+        scaledImage = convertedImage.scaled(600, 400, qtc.Qt.KeepAspectRatio)
+        qt_img = qtg.QPixmap.fromImage(scaledImage)
+        
+        # update to screen:
+        self.screen.setPixmap(qt_img)
         
 if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
